e12449d1671dc27edf459d0418caf9a4cd1b9909
李茂杰 <1136462573@qq.com>
2019-03-23 19:33:20 +0800

    beanUtils

---
+++ src/main/java/com/dreamf/common/BeanUtil.java
@@ -0,0 +1,39 @@
+package com.dreamf.common;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.BeanUtilsBean;
+
+public class BeanUtil {
+    
+    /**
+     * class 必须要有构造方法
+     * 
+     * @param orig 来源
+     * @param dest 需要返回的class对象
+     * @return class.newInstance
+     */
+    public static <T> T copy(Object orig, Class dest) {
+        T t = null;
+        try {
+            t = (T) dest.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("the class : " + dest + " don`t have the default construst");
+        }
+        try {
+            BeanUtils.copyProperties(t, orig);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            // ignore
+        }
+        return t;
+    }
+    
+    public static void copyProperties(Object dest, Object orig) {
+        try {
+            BeanUtils.copyProperties(dest, orig);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            // ignore
+        }
+    }
+}
